02e156f97757b5d4b1df16ab5bc3e049bd15fcc3
dom <domalbano35@gmail.com>
2022-10-15 10:37:42 -0400

    changed search comp. file name and removed unused style sheet

--- src/components/FetchWeather.js
+++ src/components/FetchWeather.js
@@ -1,49 +1,129 @@
-import '../styles/fetchWeather.css'
+import React, { useState, } from 'react';
+import * as api from '../apiScript/weatherAPI';
+import SearchField from './SearchField';
+import '../styles/input.css'
+import CurrentWeather from './Current';
+import HourlyWeather from './Hourly';
 
-const REACT_APP_API = process.env.REACT_APP_API
-const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY
+export default function FetchWeather() {
 
-function getInput() {
-    let userInput = document.querySelector('#cityInput').value.toLowerCase().trim();
-    console.log(userInput);
-    if (userInput) {
-        return userInput
-    } else if (userInput === '') {
-        alert('please enter a valid city name');
-    } else {
-        return '';
+    const [state, setState] = useState({
+        value: '',
+        location: '',
+        current: {},
+        hourlyWeather: [],
+        weeklyWeather: [],
+        loading: false,
+        error: false,
+    })
+
+    const handleInputChange = e => {
+        setState({
+            ...state,
+            value: e.target.value
+        })
     }
-};
 
-function buildGeoApi(userInput) {
-    return `${REACT_APP_API}/geo/1.0/direct?q=${userInput}&limit=5&appid=${REACT_APP_API_KEY}`;
-};
+    async function showMeTheWeather(userInput) {
 
-function buildWeatherAPI(coords) {
-    return `${REACT_APP_API}/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&units=imperial&exclude=minutely&appid=${REACT_APP_API_KEY}`;
+            const gettingCoords = api.buildGeoApi(userInput);
+            const coords = await api.getCoordinates(gettingCoords);
+            console.log(coords);
+            console.log(coords.name);
+            const gettingWeather = api.buildWeatherAPI(coords);
+            const weather = await api.getWeatherData(gettingWeather);
+            console.log(weather);
+            
+            
+            const locationData = coords;
+            const weatherData = weather;
+            const hourlyData = weather.hourly;
 
-};
+            const current = {
+                city: locationData.name,
+                state: locationData.state,
+                country: locationData.country,
+                tempurature: weatherData.current.temp,
+                realFeel: weatherData.current.feels_like,
+                weatherDescription: weatherData.current.weather[0].description,
+                icon: weatherData.current.weather[0].icon,
+                uvIndex: weatherData.current.uvi
+                };
+            
+            console.log(current);
 
-async function getCoordinates(geoApi) {
-    const response = await fetch(geoApi);
-    const geoCoords = await response.json();
-    let coords = geoCoords[0]
-    console.log(coords);
-    return coords
-};
+            
 
-async function getWeatherData(weatherApi) {
-    const response = await fetch(weatherApi);
-    const weather = await response.json();
-    console.log(weather);
-    return weather;
+            const hourlyWeather = hourlyData.map((data, index) => {
+                return{
+                    key: index,
+                    tempurature: data.temp,
+                    realFeel: data.feels_like,
+                    description: data.weather[0].description,
+                    icon: data.weather[0].icon
+
+                }
+            } )
+            console.log(hourlyWeather);
+
+            setState({
+                ...state,
+                current,
+                hourlyWeather,
+                loading: false,
+                error: false
+            })
+    }
+
+
+    const handleInputSubmit = e => {
+        console.log('you submitted me');
+
+        e.preventDefault();
+
+        setState({
+            ...state,
+            location: e.target.value,
+            loading: true
+        })
+
+        console.log(state.value);
+
+        showMeTheWeather(state.value)
+
+    }
+
+
+
+    // const data = state.loading ? <div>I am loading</div> : <div>Is it currently {state.current.tempurature} degrees in {state.current.city}</div>
+    // const hourly = isLoading ? <div></div> : {storage.map}    
+
+
+
+    return (
+        <div className='fetching-weather'>
+
+            <div className='input-search'>
+                < SearchField
+                    value={state.value}
+                    data={state}
+                    change={handleInputChange}
+                    submit={handleInputSubmit}
+                />
+            </div>
+
+            {/* <div className='weatherGreeting'>{data}</div> */}
+            <div>
+                < CurrentWeather 
+                today={state.current} 
+                />
+            </div>
+            <div>
+                < HourlyWeather 
+                // today={state.current} 
+                hourly={state.hourlyWeather} 
+                />
+            </div>
+        </div>
+    );
 }
-
-export {
-    getInput,
-    buildGeoApi,
-    buildWeatherAPI,
-    getCoordinates,
-    getWeatherData
-}
-
