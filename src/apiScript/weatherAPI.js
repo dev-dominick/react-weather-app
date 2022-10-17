55f9ddefe042ed3b695df74d1063846b362e4cf6
dom <domalbano35@gmail.com>
2022-10-17 18:32:49 -0400

    changes

--- src/apiScript/weatherAPI.js
+++ src/apiScript/weatherAPI.js
@@ -4,50 +4,38 @@ const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY
 
 
 function buildGeoApi(userInput) {
-
-    console.log('building geo api');
-
+    console.log('building geo api from input');
     return `${REACT_APP_API}/geo/1.0/direct?q=${userInput}&limit=5&appid=${REACT_APP_API_KEY}`;
 
 };
 
 async function getCoordinates(geoApi) {
-    console.log('wiating for coords');
+    console.log('waiting for coords response');
     const response = await fetch(geoApi);
-    console.log('waiting for api call');
     const geoCoords = await response.json();
     let coords = geoCoords[0]
-    console.log(coords);
     return coords
 };
 
 function buildWeatherAPI(coords) {
+    console.log('building weather api with coords');
     return `${REACT_APP_API}/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&units=imperial&exclude=minutely&appid=${REACT_APP_API_KEY}`;
 
 };
 
 async function getWeatherData(weatherApi) {
+    console.log('waiting for weather respsonse');
     const response = await fetch(weatherApi);
     const weather = await response.json();
     console.log(weather);
     return weather;
-}
+};
 
-// async function showMeTheWeather(userInput) {
-//     const gettingCoords = buildGeoApi(userInput);
-//     const coords = await getCoordinates(gettingCoords);
-//     const gettingWeather = buildWeatherAPI(coords);
-//     const weather = await getWeatherData(gettingWeather);
-//     console.log(weather);
-//     return weather
-// }
 
 export {
-    // getInput,
     buildGeoApi,
     buildWeatherAPI,
     getCoordinates,
     getWeatherData
-    // showMeTheWeather
-}
+};
 
