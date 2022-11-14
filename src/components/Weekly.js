00f623a8d5b382d883f381a6179291e4a3d2a778
dom <domalbano35@gmail.com>
2022-11-14 13:55:44 -0500

    set days of week for weekly data

--- src/components/Weekly.js
+++ src/components/Weekly.js
@@ -2,7 +2,7 @@ import React from 'react'
 import '../styles/weekly.css'
 
 
-export default function WeeklyWeather({ weekly }) {
+export default function WeeklyWeather({ weekly, day }) {
     return (
 
         <div className='weekly-display-container'>
@@ -10,6 +10,7 @@ export default function WeeklyWeather({ weekly }) {
             {weekly.map((data, index) => (
                 <div key={index} style={{ height: '100%', width: '100%' }}>
                     <div className='weekly-display'>
+                        <p>today is { data.dateWeekDay }</p>
                         <p id='text'>Weather today high of {data.highTemp} °f</p>
                         <p id='text'>Weather today low of {data.lowTemp} °f</p>
                     </div>
