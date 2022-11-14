c1ff7ad5b66a1495732deb347722d306cacbc0e8
dom <domalbano35@gmail.com>
2022-11-14 13:54:27 -0500

    set sunrise and sunset

--- src/components/Current.js
+++ src/components/Current.js
@@ -4,18 +4,19 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 
-export default function CurrentWeather({  today  }) {
+export default function CurrentWeather({ today, time }) {
     return (
         <>
             <Box sx={{ minWidth: 275, display: 'flex', justifyContent: 'center', paddingBottom: '3rem' }}>
-                <Card variant="outlined" sx={{width: '50vw', display: 'flex', justifyContent: 'center'}}>
+                <Card variant="outlined" sx={{ width: '50vw', display: 'flex', justifyContent: 'center' }}>
                     <CardContent>
-
+                        
                         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                            Today in {today.city}, {today.state}, {today.country}
+                            Weather in {today.city}, {today.state}, {today.country}
+                            <br></br>as of {time}
                         </Typography>
 
-                        <Typography variant="h5" component="div">
+                        <Typography variant="p" component="div">
                             Current Weather is {today.tempurature}°f
                         </Typography>
 
@@ -25,6 +26,12 @@ export default function CurrentWeather({  today  }) {
 
                         <Typography variant="body2">
                             {today.weatherDescription}
+                        </Typography>
+                        <Typography variant="body2">
+                            Sunrise at {today.sunrise} today
+                        </Typography>
+                        <Typography variant="body2">
+                            Sunset at {today.sunset} today
                         </Typography>
 
                     </CardContent>
